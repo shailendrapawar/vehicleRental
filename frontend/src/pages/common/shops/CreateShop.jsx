e612602684f0f2324030419ee1e865f2b0bf0da3
shailendr pawar <shailendrapawar980@gmail.com>
2025-12-20 23:41:16 +0530

    added: shopImage UI

--- frontend/src/pages/common/shops/CreateShop.jsx
+++ frontend/src/pages/common/shops/CreateShop.jsx
@@ -3,28 +3,36 @@ import { FaShop } from "react-icons/fa6";
 import Header from "../../../components/header/Header";
 import { useSelector } from "react-redux";
 import InputBox from "../../../components/inputBox/InputBox";
+import { useRef } from "react";
+
+import { MdAddPhotoAlternate } from "react-icons/md";
+
 function CreateShop() {
 
   const { currentTheme } = useSelector(s => s.theme);
+  const gstInputRef = useRef(null);
+  const shopPhotosInputRef = useRef(null);
 
   const handleCreateShop = async () => {
 
   }
+
   return (
+
     <div className=" h-full w-full rounded-md relative">
+
       <Header title="Shop Management" action={handleCreateShop} actionTitle="Submit" />
 
-      <section className={"w-full h-120  mt-5 rounded-md bg-gray-500 grid grid-cols-1 grid-rows-2 gap-4"}
-
+      <section className={"w-full h-120 p-2 sm:h-60  mt-5 rounded-md bg-gray-500 grid grid-cols-1 grid-rows-2 sm:grid-cols-2 sm:grid-rows-1 gap-4"}
         style={{
           backgroundColor: currentTheme.cardBackground,
-          border: `1px solid 5px ${currentTheme.border}`,
+          border: `1px solid ${currentTheme.border}`,
           boxShadow: `2px 2px 5px ${currentTheme.border}`
         }}
       >
 
-        <div className="bg-red- flex flex-col justify-evenly px-2 bg-green-500"
-
+        {/* // basic detils  */}
+        <div className="flex flex-col justify-evenly px-2 "
         >
           <h3>Basic Details:</h3>
           <div className="grid grid-cols-2 grid-rows-3 gap-2">
@@ -36,6 +44,21 @@ function CreateShop() {
               placeholder={"Enter GST Number"}
               border={`2px solid ${currentTheme.border}`}
             />
+
+            <input type="file" className="gstBill-input h-6 col-span-2  text-xs hidden"
+              ref={gstInputRef}
+            ></input>
+
+            <div className="col-span-2 text-white h-8 rounded-md text-sm flex justify-center items-center cursor-pointer"
+              style={{
+                backgroundColor: currentTheme.secondary
+              }}
+              title="Upload GST bill"
+              onClick={(e) => gstInputRef?.current?.click()}
+            >
+              Select GST bill image
+            </div>
+
             <textarea className="resize-none col-span-2 row-span-2 p-1 text-xs outline-none rounded-md"
               placeholder="Enter some description for your shop."
               style={{
@@ -53,20 +76,28 @@ function CreateShop() {
 
         </div>
 
-        <div className="bg-red-500">
+        {/* // updaloding SHOP photos  */}
+        <div className="rounded-md flex justify-center items-center"
+          style={{
+            border: `2px solid ${currentTheme.secondary}`
+          }}
+        >
+          <input type="file" className="gstBill-input h-6 col-span-2  text-xs hidden"
+            ref={shopPhotosInputRef}
+          ></input>
 
+          <span className="flex flex-col items-center justify-center cursor-pointer"
+            onClick={(e) => shopPhotosInputRef?.current?.click()}
+          >
+            <MdAddPhotoAlternate className="h-15 w-15" style={{ color: currentTheme.secondary }} />
+            <h3 >Add Shop Images</h3>
+          </span>
         </div>
-
       </section>
-
-
-
-
 
 
       <section className={"w-full h-auto p-2 mt-5 rounded-md bg-gray-500"}
         style={{
-
         }}
       >
       </section>
